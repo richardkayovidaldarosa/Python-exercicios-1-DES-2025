6f6f64219f04da340f837edbed8af833eebf5b7a
richardkayovidaldarosa <richard.vidal.rosa@escola.pr.gov.br>
2025-06-02 14:11:52 +0000

    exercicio02 quase feito

--- exercicio02.py
+++ exercicio02.py
@@ -3,3 +3,12 @@
 #  Se alguma tarefa estiver com o tempo de entrega negativo, deve-se exibir uma mensagem de 
 # erro e não somar o total.
 
+Dia_01 = int (input)("Dias para atividade 01")
+Dia_02 = int (input)("Dias para atividade 02")
+Dia_03 = int (input)("Dias para atividade 03")
+
+if Dia_<0 or Dia02 <0 or Dia_03 <0:
+    print ("erro numeros negativos")
+
+Else:
+     soma
