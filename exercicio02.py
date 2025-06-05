be7930a896e76e66d21d2fcd668f99c197f3ce63
richardkayovidaldarosa <richard.vidal.rosa@escola.pr.gov.br>
2025-06-05 14:47:51 +0000

    quase feito

--- exercicio02.py
+++ exercicio02.py
@@ -7,8 +7,11 @@ Dia_01 = int (input)("Dias para atividade 01")
 Dia_02 = int (input)("Dias para atividade 02")
 Dia_03 = int (input)("Dias para atividade 03")
 
-if Dia_<0 or Dia02 <0 or Dia_03 <0:
+if Dia_01<0 or Dia_02 <0 or Dia_03 <0:
     print ("erro numeros negativos")
 
-Else:
-     soma
+else:
+    soma 
+    
+    
+     
